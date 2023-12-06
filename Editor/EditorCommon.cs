271d3749fa7f11efde186678298008a83b6f1c54
Himanshu Parchand <himan2104@gmail.com>
2023-12-07 01:55:52 +0530

    test d02

--- Editor/EditorCommon.cs
+++ Editor/EditorCommon.cs
@@ -1,18 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using UnityEditor;
 
-public class EditorCommon : MonoBehaviour
+public class EditorCommon : Editor
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
