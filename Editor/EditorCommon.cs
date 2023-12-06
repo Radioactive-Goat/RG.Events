e6664922a59646443b9eea03784beb6b53fa6ee5
Himanshu Parchand <himan2104@gmail.com>
2023-12-07 02:25:27 +0530

    test d03

--- Editor/EditorCommon.cs
+++ Editor/EditorCommon.cs
@@ -2,5 +2,9 @@ using UnityEditor;
 
 public class EditorCommon : Editor
 {
-    
+    [MenuItem("Radioactive Goat/Event System/Create Event System")]
+    public static void CreateEventSystem()
+    {
+
+    }
 }
